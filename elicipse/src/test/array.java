8ab5514ab10aa9be96a5f14808d6d95242823980
komincheol <jckmm@naver.com>
2017-10-18 18:13:37 +0900

    update

---
+++ elicipse/src/test/array.java
@@ -0,0 +1,21 @@
+package test;
+
+import java.util.Scanner;
+
+public class array {
+public static void main(String[] args) {
+
+	String[][] names= new String[2][30];
+	int[][][] scores = new int[2][30][6];
+	double[][] avgs= new double[2][30];
+	char con= 'Y';
+	int [] cnts= new int[2];
+	Scanner scanner = new Scanner(System.in);
+	
+	do {
+		
+	}while(Character.toLowerCase(con)=='y');
+	
+	
+}
+}
